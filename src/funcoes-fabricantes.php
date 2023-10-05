42d4fa129b3b4f1cd7642b7474e3270738eae812
André Marques <andre.m.d.abreu@gmail.com>
2023-10-05 09:22:33 -0300

    Update funcoes-fabricantes.php

--- src/funcoes-fabricantes.php
+++ src/funcoes-fabricantes.php
@@ -7,20 +7,7 @@ require_once "conecta.php";
 
 
 
-function lerUmFabricante(PDO $conexao, int $idFabricante):array {
-    $sql = "SELECT * FROM fabricantes WHERE id = :id";
 
-    try {
-        $consulta = $conexao->prepare($sql);
-        $consulta->bindValue(":id", $idFabricante, PDO::PARAM_INT);
-        $consulta->execute();
-        $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
-    } catch (Exception $erro) {
-        die("Erro ao carregar: ".$erro->getMessage());
-    }
-
-    return $resultado;
-} 
 
 
 function atualizarFabricante(PDO $conexao, string $nome, int $id):void {
@@ -34,7 +21,7 @@ function atualizarFabricante(PDO $conexao, string $nome, int $id):void {
     } catch (Exception $erro) {
         die("Erro ao atualizar: ".$erro->getMessage());
     }
-} 
+}
 
 function excluirFabricante(PDO $conexao, int $id):void {
     $sql = "DELETE FROM fabricantes WHERE id = :id";
